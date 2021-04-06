8b02a53d1ed1f572fc9da62b5b78bb6e590ab7a4
lumilitas <lucas23121995>
2021-04-06 13:43:24 -0300

    adicionado eslint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
-import express, { response } from "express";
+import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hell Galera' });
-});
+app.get('/', (request, response) => response.json({ message: 'Hell Galera' }));
 
 app.listen(3333, () => {
   console.log('👽Server started');
