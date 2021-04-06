73332c42212dc76ce1bf48f1d256de9f453f29c7
lumilitas <lucas23121995>
2021-04-06 14:27:53 -0300

    configurado o debug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,15 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  response.json({ message: 'Hell Galera' });
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json({ message: 'Hell Galera' });
 });
 
 export default routes;
