f17802f169624b18949bbbf7f1622b2fa4a37fac
lumilitas <lucas23121995>
2021-04-08 08:39:45 -0300

    implementando as rotas de tarefas com array

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import usersRouter from './user.routes';
 import sessionsRouter from './session.routes';
+import tasksRouter from './task.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/tasks', tasksRouter);
 
 export default routes;
---
+++ src/routes/task.routes.ts
@@ -0,0 +1,64 @@
+import { request, response, Router } from 'express';
+import { uuid } from 'uuidv4';
+
+const tasksRouter = Router();
+
+const tasks = [];
+
+tasksRouter.post('/', async (request, response) => {
+  const { name, concluded, responsible } = request.body;
+
+  const newTask = {
+    id: uuid(),
+    name,
+    concluded,
+    responsible,
+  };
+
+  tasks.push(newTask);
+
+  return response.json(newTask);
+});
+
+tasksRouter.get('/', async (request, response) => {
+  return response.json(tasks);
+});
+
+tasksRouter.put('/:id', async (request, response) => {
+  const { id } = request.params;
+  const { name, concluded } = request.body;
+
+  const tasksIndex = tasks.findIndex(task => task.id === id);
+
+  if (tasksIndex < 0) {
+    return response.status(400).json({ message: 'tarefa não encontrada!' });
+  }
+
+  const responsible = tasks[tasksIndex].responsible;
+  const newTask = {
+    id,
+    name,
+    responsible,
+    concluded,
+  };
+
+  tasks[tasksIndex] = newTask;
+
+  return response.json(newTask);
+});
+
+tasksRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const tasksIndex = tasks.findIndex(task => task.id === id);
+
+  if (tasksIndex < 0) {
+    return response.status(400).json({ message: 'tarefa não encontrada!' });
+  }
+
+  tasks.splice(tasksIndex, 1);
+
+  return response.status(204).send();
+});
+
+export default tasksRouter;
