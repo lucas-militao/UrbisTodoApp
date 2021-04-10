3010d99c467d5574c3a8088c32ee1c15a347dc45
lumilitas <lucas23121995>
2021-04-10 19:15:04 -0300

    Estilizando mais os inputs

--- frontend/urbis-todo-web/src/components/Input/styles.ts
+++ frontend/urbis-todo-web/src/components/Input/styles.ts
@@ -1,10 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
+
+  border: 2px solid #e0e0e0;
+  color: #e0e0e0;
 
   display: flex;
   align-items: center;
@@ -13,18 +20,30 @@ export const Container = styled.div`
     margin-top: 8px;
   }
 
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #000000;
+      border-color: #000000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #000000;
+    `}
+
   input {
     flex: 1;
     background: transparent;
     border: 0;
 
     &::placeholder {
-      color: #666360;
+      color: #e0e0e0;
     }
   }
 
   svg {
     margin-right: 16px;
-    color: #666360;
   }
 `;
